e4853dc68798fae003a553a57c3f69f8be5c714e
Zan-afk <aniket.zanje01@gmail.com>
2024-11-25 12:46:36 +0530

    created demo project

---
+++ src/main/java/com/example/githubpractice/GithubpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubpractice/GithubpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
